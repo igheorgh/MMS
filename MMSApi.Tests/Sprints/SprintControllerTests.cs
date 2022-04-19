577057f8ee08593c86a318eca78dee2e19be27b9
Clara <claragabrielav@gmail.com>
2022-04-19 12:55:58 +0300

    add tests for SprintController

---
+++ MMSApi.Tests/Sprints/SprintControllerTests.cs
@@ -0,0 +1,178 @@
+﻿using DataLibrary.DTO;
+using DataLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using MMSAPI;
+using MMSAPI.Controllers;
+using MMSAPI.Repository;
+using MMSAPI.Validations;
+using MMSAPI.Validations.Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MMSApi.Tests.Sprints
+{
+    public class SprintControllerTests : IClassFixture<TestFixture<Startup>>
+    {
+        private SprintController Controller;
+
+        private ISprintRepository fakeRepository;
+
+        private int IntitialSprintCount = 20;
+
+        public SprintControllerTests(TestFixture<Startup> fixture)
+        {
+            fakeRepository = FakeSprintRepository.GetFakeSprintRepository(IntitialSprintCount);
+
+            var entityUpdate = new Mock<IEntityUpdateHandler>();
+
+            entityUpdate.Setup(x => x.Update<Sprint>(It.IsAny<Sprint>()))
+                .Returns((Sprint sprint) =>
+                    new EntityHandlerResult<Sprint>(null, sprint));
+
+            Controller = new SprintController(fakeRepository, null, entityUpdate.Object);
+        }
+
+        [Fact]
+        public void CreateSprint()
+        {
+            //Arrange
+            var newSprint = SprintHelper.GetTestSprint(true);
+            //Act
+            var actionResult = Controller.Create(SprintDTO.FromModel(newSprint)) as OkObjectResult;
+            //Assert
+            Assert.NotNull(actionResult);
+            var responseObject = actionResult.Value as SprintDTO;
+            Assert.NotNull(responseObject);
+            Assert.Equal(newSprint.End_Date, responseObject.End_Date);
+            Assert.Equal(newSprint.Start_Date, responseObject.Start_Date);
+            Assert.Equal(newSprint.Goal, responseObject.Goal);
+            Assert.Equal(newSprint.Name, responseObject.Name);
+        }
+
+        [Fact]
+        public void CreateSprintFromEmptyData()
+        {
+            //Arrange
+
+            //Act
+            var actionResult = Controller.Create(null) as BadRequestResult;
+            //Assert
+            Assert.NotNull(actionResult);
+        }
+
+        [Theory]
+        [InlineData("2")]
+        [InlineData("1")]
+        [InlineData("4")]
+        [InlineData("0")]
+        public void DeleteSprintById(string id)
+        {
+            //Arrange
+            int initialCount = FakeSprintRepository.Sprints.Count;
+            //Act
+            var actionResult = Controller.Delete(id) as OkObjectResult;
+            //Assert
+            Assert.NotNull(actionResult);
+            Assert.True((bool)actionResult.Value);
+            Assert.Equal(FakeSprintRepository.Sprints.Count, initialCount - 1);
+        }
+
+        [Theory]
+        [InlineData("-1")]
+        [InlineData("20000")]
+        [InlineData("-21")]
+        [InlineData("21")]
+        public void DeleteUnexistingSprintById(string id)
+        {
+            //Arrange
+            int initialCount = FakeSprintRepository.Sprints.Count;
+            //Act
+            Controller.Delete(id);
+            //Assert
+            Assert.Equal(FakeSprintRepository.Sprints.Count, initialCount);
+        }
+
+        [Fact]
+        public void DeleteSprintWithNullAsId()
+        {
+            //Arrange
+            int initialCount = FakeSprintRepository.Sprints.Count;
+            //Act
+            var actionResult = Controller.Delete(null) as BadRequestResult;
+            //Assert
+            Assert.NotNull(actionResult);
+        }
+
+        [Fact]
+        public void GetExistingSprintById()
+        {
+            //Arrange
+            var sprint = FakeSprintRepository.Sprints[1];
+            //Act
+            var actionResult = Controller.GetById(sprint.Id) as OkObjectResult;
+            //Assert
+            Assert.NotNull(actionResult);
+            var response = actionResult.Value as SprintDTO;
+            Assert.Equal(sprint.Id, response.Id);
+        }
+
+        [Fact]
+        public void GetUnexistingSprintById()
+        {
+            //Arrange
+            var id = "someid";
+            //Act
+            var actionResult = Controller.GetById(id) as OkObjectResult;
+            //Assert
+            Assert.NotNull(actionResult);
+            var response = actionResult.Value as SprintDTO;
+            Assert.Null(response);
+        }
+
+        [Fact]
+        public void GetSprintByIdWithNullId()
+        {
+            //Act
+            var actionResult = Controller.GetById(null) as BadRequestResult;
+            //Assert
+            Assert.NotNull(actionResult);
+        }
+
+        [Fact]
+        public void GetAllSprints()
+        {
+            //Act
+            var actionResult = Controller.GetAll() as OkObjectResult;
+            //Assert
+            Assert.NotNull(actionResult);
+            var allSprints = actionResult.Value as List<Sprint>;
+            Assert.Equal(FakeSprintRepository.Sprints.Count, allSprints.Count);
+        }
+
+        [Fact]
+        public void EditSprint()
+        {
+            //Arrange
+            var sprint = SprintHelper.GetTestSprint();
+            //Act
+            var actionResult = Controller.Update(SprintDTO.FromModel(sprint)) as ObjectResult;
+            //Assert
+            Assert.NotNull(actionResult);
+            Assert.Equal(200, actionResult.StatusCode);
+            var response = actionResult.Value as EntityHandlerResult<Sprint>;
+            Assert.Equal(sprint.Id, response.SuccessResult.Id);
+        }
+
+        [Fact]
+        public void EditNullSprint()
+        {
+            //Act
+            var actionResult = Controller.Update(null) as BadRequestResult;
+            //Assert
+            Assert.NotNull(actionResult);
+        }
+    }
+}
---
+++ MMSApi.Tests/Sprints/SprintHelper.cs
@@ -0,0 +1,41 @@
+﻿using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+
+namespace MMSApi.Tests.Sprints
+{
+    public static class SprintHelper
+    {
+        public static Sprint GetTestSprint(bool emptyId = false)
+        {
+            var randDays = (new Random()).Next(14, 14 * 12);
+            return new Sprint
+            {
+                Start_Date = DateTime.Now.AddDays(randDays),
+                End_Date = DateTime.Now.AddDays(randDays - 14),
+                Goal = $"Sprint de acum {randDays} Goal",
+                Id = !emptyId ? Guid.NewGuid().ToString() : null,
+                Name = $"Sprint de acum {randDays}",
+                Tasks = new List<AppTask>()
+            };
+        }
+
+        public static List<Sprint> GetTestData(int count)
+        {
+            var returnData = new List<Sprint>();
+            for(int i = 0; i < count; i++)
+            {
+                returnData.Add(new Sprint
+                {
+                    Start_Date = DateTime.Now.AddDays(14 * (count - i)),
+                    End_Date = DateTime.Now.AddDays(14 * (count - i) - 14),
+                    Goal = $"Sprint {i} Goal",
+                    Id = i.ToString(),
+                    Name = $"Sprint {i}",
+                    Tasks = new List<AppTask>()
+                });
+            }
+            return returnData;
+        }
+    }
+}
